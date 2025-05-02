5cdff9371c86d6d52423e9ad0831cd39c8035625
Bernd Schoolmann <mail@quexten.com>
2025-05-02 12:55:40 +0200

    Fix build

--- src/session_bind.rs
+++ src/session_bind.rs
@@ -118,8 +118,9 @@ fn verify_ecdsa_signature(
 
 pub(crate) async fn respond_extension_session_bind<
     'a,
-    A: Agent<I> + Send + Sync + 'static,
+    A: Agent<I, K> + Send + Sync + 'static,
     I: Clone,
+    K,
 >(
     agent: &mut A,
     r: &mut Position<'a>,
