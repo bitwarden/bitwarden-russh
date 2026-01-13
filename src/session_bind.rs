554ea00376d627cffc9ab713a95abce5f9c41383
neuronull <9162534+neuronull@users.noreply.github.com>
2026-01-13 15:22:52 -0700

    Fix clippy warning about deprecated method

--- src/session_bind.rs
+++ src/session_bind.rs
@@ -60,7 +60,7 @@ fn verify_rsa_signature(
         verifying_key
             .verify(
                 Pkcs1v15Sign::new::<sha2::Sha256>(),
-                sha2::Sha256::digest(session_identifier).as_slice(),
+                sha2::Sha256::digest(session_identifier).as_ref(),
                 signature,
             )
             .map_err(Into::into)
@@ -68,7 +68,7 @@ fn verify_rsa_signature(
         verifying_key
             .verify(
                 Pkcs1v15Sign::new::<sha2::Sha512>(),
-                sha2::Sha512::digest(session_identifier).as_slice(),
+                sha2::Sha512::digest(session_identifier).as_ref(),
                 signature,
             )
             .map_err(Into::into)
